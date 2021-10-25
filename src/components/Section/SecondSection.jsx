faeb1aa71155106bfe3dcfc04d08e913b2a3eb02
Hadeymike <adeyemitimmy@gmail.com>
2021-10-25 09:54:02 +0100

    third section modified

--- src/components/Section/SecondSection.jsx
+++ src/components/Section/SecondSection.jsx
@@ -11,7 +11,7 @@ function SecondSection() {
         <h1 className="second_heading">Features We Provide For You</h1>
         <p className="paragraph">Curabitur tempor justo non turpis malesuada cursus. Mauris ac libero eu sem finibus lacinia nec pulvinar.</p>
         <p>
-        <span> <p className="nav-item"></p> Curabitur tempor justo</span>
+        <span> <p className=""></p> Curabitur tempor justo</span>
        
         <li>Curabitur tempor justo.</li>
         <li>Curabitur tempor justo</li>
